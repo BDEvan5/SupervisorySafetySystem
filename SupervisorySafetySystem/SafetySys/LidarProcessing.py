108cc37bf2e0aa4b8e17690328cbe56f6e6f8cd4
Benjamin <benjaminevans316@gmail.com>
2021-06-29 09:33:51 +0200

    clean segmentation algorithm

--- SupervisorySafetySystem/SafetySys/LidarProcessing.py
+++ SupervisorySafetySystem/SafetySys/LidarProcessing.py
@@ -1,61 +1,30 @@
 import numpy as np
 from numba import njit 
 import matplotlib.pyplot as plt
+from scipy import signal 
 
 from SupervisorySafetySystem.SafetySys.safety_utils import *
 
-def segment_lidar_scan(scan):
-    """
-    Take a lidar scan and return a set of critical x, y points
-     """
-    pass
+
 
 def load_lidar():
     scan = np.load("lidar_scan.npy")
     return scan
 
-# @njit(cache=True)
+
 def segment_lidar_scan(scan):
-    """ 
-    Takes a lidar scan and reduces it to a set of points that make straight lines 
-    TODO: possibly change implmentation to work completely in r, ths 
-    """
+    """Returns a set of critical x, y points"""
+    # scan = signal.medfilt(scan, 19)
+
     xs, ys = convert_scan_xy(scan)
     diffs = np.sqrt((xs[1:]-xs[:-1])**2 + (ys[1:]-ys[:-1])**2)
     scaled_diffs = diffs / scan[:-1]
 
     i_pts = [0]
-    d_thresh = 0.08
+    scale_d_thresh = 0.08
+    distance_threshold = 0.08
     for i in range(len(diffs)):
-        if diffs[i] > d_thresh:
-            i_pts.append(i)
-            i_pts.append(i+1)
-    i_pts.append(len(scan)-1)
-
-
-    if len(i_pts) < 3:
-        i_pts.append(np.argmax(scan))
-        
-    i_pts = np.array(i_pts)
-    x_pts = xs[i_pts]
-    y_pts = ys[i_pts]
-
-    return x_pts, y_pts
-
-def segment_lidar_scan_scale(scan):
-    """ 
-    Takes a lidar scan and reduces it to a set of points that make straight lines 
-    TODO: possibly change implmentation to work completely in r, ths 
-    """
-    xs, ys = convert_scan_xy(scan)
-    diffs = np.sqrt((xs[1:]-xs[:-1])**2 + (ys[1:]-ys[:-1])**2)
-    scaled_diffs = diffs / scan[:-1]
-
-    i_pts = [0]
-    d_thresh = 0.05
-    for i in range(len(diffs)):
-        if scaled_diffs[i] > d_thresh and diffs[i] > 0.08:
-        # if scaled_diffs[i] > d_thresh:
+        if scaled_diffs[i] > scale_d_thresh and diffs[i] > distance_threshold:
             i_pts.append(i)
             i_pts.append(i+1)
     i_pts.append(len(scan)-1)
@@ -65,6 +34,10 @@ def segment_lidar_scan_scale(scan):
     y_pts = ys[i_pts]
 
     return x_pts, y_pts
+
+def test_numpy_filters(scan):
+    scan = signal.medfilt(scan, 19)
+    return convert_scan_xy(scan)
 
 @njit(cache=True)
 def convert_scan_xy(scan):
@@ -98,10 +71,17 @@ def run_lidar_process():
 
     # plt.figure(4)
     # plt.title("Segmented Scan")
-    x_pts, y_pts = segment_lidar_scan(scan)
-    plt.plot(x_pts, y_pts, 'x')
+    # x_pts, y_pts = segment_lidar_scan(scan)
+    # plt.plot(x_pts, y_pts, 'x')
 
-    x_pts_scale, y_pts_scale = segment_lidar_scan_scale(scan)
+    # x_pts, y_pts = segment_lidar_scan_avg(scan)
+    # plt.plot(x_pts, y_pts, 'x')
+
+    # x_pts_scale, y_pts_scale = segment_lidar_scan_avg(scan)
+    # plt.figure(1)
+    # plt.plot(x_pts_scale, y_pts_scale, '-+', markersize=20)
+
+    x_pts_scale, y_pts_scale = segment_lidar_scan(scan)
     plt.plot(x_pts_scale, y_pts_scale, '-+', markersize=20)
 
     # plt.figure(4)
@@ -109,6 +89,10 @@ def run_lidar_process():
     # scaled_diffs = diffs / scan[:-1]
     # plt.plot(x_ints[:-1], scaled_diffs)
     # plt.ylim([0, 0.2])
+
+    # x_f, y_f = test_numpy_filters(scan)
+    # plt.figure(2)
+    # plt.plot(x_f, y_f)
 
 
     plt.show()
