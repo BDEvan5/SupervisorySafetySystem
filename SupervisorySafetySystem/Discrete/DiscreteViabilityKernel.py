eeb15c6360e51ee17dccb67a6617bb8877081ff1
Benjamin <benjaminevans316@gmail.com>
2021-10-11 14:34:31 +0200

    Last attempt with speed before removal

--- SupervisorySafetySystem/Discrete/DiscreteViabilityKernel.py
+++ SupervisorySafetySystem/Discrete/DiscreteViabilityKernel.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 class ViabilityKernel:
     def __init__(self, width=1, length=5):
-        self.resolution = 20 # pts per meter
+        self.resolution = 10 # pts per meter
         self.width = width
         self.length = length
         self.n_x = self.resolution * self.width
@@ -22,7 +22,11 @@ class ViabilityKernel:
         self.n_modes = None
         self.build_qs()
 
+        #TODO: add modes as states later. For the moment, assume no dynamic window. All velocities are instantly reachable.
+        # note: todo that I will have to have two dimensions for n_modes. One for the state and one for each possible option.
+
         self.kernel = np.zeros((self.n_x, self.n_y, self.n_phi, self.n_modes))
+        self.set_track_constraints()
 
     def build_qs(self):
         v_max = 5  
@@ -48,11 +52,11 @@ class ViabilityKernel:
             self.qs[idx, 1] = temp_qs[idx, 1]
             self.qs[idx, 0] = temp_qs[idx, 1] / L * np.tan(temp_qs[idx,0]) 
 
-        plt.figure()
-        for pt in self.qs:
-        # for pt in temp_qs:
-            plt.plot(pt[0], pt[1], 'ro')
-        plt.show()
+        # plt.figure()
+        # for pt in self.qs:
+        # # for pt in temp_qs:
+        #     plt.plot(pt[0], pt[1], 'ro')
+        # plt.show()
 
         return np.copy(self.qs)
 
@@ -60,22 +64,71 @@ class ViabilityKernel:
         # left and right wall
         self.kernel[0, :, :, :] = 1
         self.kernel[-1, :, :, :] = 1
+        self.kernel[:, 32:35, :, :] = 1
 
 
-    def safe_update(self, x, y, phi, q_input, t_step=0.1):
+    def safe_update(self, x, y, phi, q_input, t_step=1):
         new_x = x + np.sin(phi) * self.qs[q_input, 1] * t_step
         new_y = y + np.cos(phi) * self.qs[q_input, 1] * t_step
         new_phi = phi + self.qs[q_input, 0] * t_step
 
-    def calculate_kernel(self):
-        for z in range(5):
+        return new_x, new_y, new_phi
+
+    def calculate_kernel(self, n_loops=1):
+        for z in range(n_loops):
+            print(f"Running loop: {z}")
             for i in range(self.n_x):
+                # print(f"Running XXXs: {i}")
                 for j in range(self.n_y):
+                    # print(f"Running YYYs: {j}")
                     for k in range(self.n_phi):
                         for l in range(self.n_modes):
-                            self.kernel[i, j, k, l] = self.check_state(i, j, k, l)
+                            self.kernel[i, j, k, l] += self.check_state(i, j, k, l)
+
+        np.save("SupervisorySafetySystem/Discrete/ViabilityKernal.npy", self.kernel)
+
+    def check_state(self, i, j, k, l):
+        x = self.xs[i]
+        y = self.ys[j]
+        phi = self.phis[k]
+        # q_input = self.qs[l]
+        new_x, new_y, new_phi = self.safe_update(x, y, phi, l)
+
+        kernal_inds = self.convert_state_to_kernel(new_x, new_y, new_phi)
+        kernel = self.kernel[kernal_inds[0], kernal_inds[1], kernal_inds[2], l]
+
+        return kernel
+
+    def convert_state_to_kernel(self, x, y, phi):
+        #TODO: very inefficient, replace with count_nonzero
+        x_ind = np.argmin(np.abs(self.xs - x))
+        y_ind = np.argmin(np.abs(self.ys - y))
+        phi_ind = np.argmin(np.abs(self.phis - phi))
+
+        return (x_ind, y_ind, phi_ind)
+
+    def load_kernel(self):
+        self.kernel = np.load("SupervisorySafetySystem/Discrete/ViabilityKernal.npy")
+
+    def view_kernel(self, phi, mode):
+        phi_ind = np.argmin(np.abs(self.phis - phi))
+        plt.figure()
+        plt.imshow(self.kernel[:, :, phi_ind, mode].T, origin='lower')
+        plt.show()
 
 
 if __name__ == "__main__":
     viab = ViabilityKernel()
-    viab.calculate_kernel()
+    # viab.load_kernel()
+    # viab.view_kernel(0, 32)
+    viab.calculate_kernel(1)
+    # viab.view_kernel(0, 32)
+    # viab.calculate_kernel(1)
+    # viab.view_kernel(0, 32)
+    # viab.calculate_kernel(1)
+    # viab.view_kernel(0, 32)
+
+    viab.view_kernel(0, 0)
+    viab.view_kernel(0, 10)
+    viab.view_kernel(0, 20)
+    viab.view_kernel(0, 32)
