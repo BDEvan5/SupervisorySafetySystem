0d20015a75e61b57654189cf3a7654c841ec79e9
Benjamin <benjaminevans316@gmail.com>
2021-10-13 13:44:08 +0200

    Fixed bug in dynamics

--- SupervisorySafetySystem/Discrete/RelativeObstacleViability.py
+++ SupervisorySafetySystem/Discrete/RelativeObstacleViability.py
@@ -31,7 +31,7 @@ class ViabilityKernel:
         self.kernel = np.zeros((self.n_x, self.n_y, self.n_phi, self.n_modes))
         self.previous_kernel = np.zeros((self.n_x, self.n_y, self.n_phi, self.n_modes))
         self.build_qs()
-        self.dynamics = build_dynamics_table(self.phis, self.qs, self.velocity, self.t_step)
+        self.dynamics = build_dynamics_table(self.phis, self.qs, self.velocity, self.t_step, self.resolution)
         self.set_mode_window_size()
         # self.build_dynamics_table()
         self.set_track_constraints()
@@ -61,7 +61,7 @@ class ViabilityKernel:
                 print("Kernel has not changed: convergence has been reached")
                 break
             self.previous_kernel = np.copy(self.kernel)
-            self.kernel = kernel_loop(self.kernel, self.xs, self.ys, self.phis, self.qs, self.mode_window_size, self.n_modes, self.dynamics, self.x_offset, self.y_offset, self.phi_range, self.resolution, self.half_block)
+            self.kernel = kernel_loop(self.kernel, self.xs, self.ys, self.phis, self.qs, self.mode_window_size, self.n_modes, self.dynamics)
 
         np.save("SupervisorySafetySystem/Discrete/RelativeObsKernal.npy", self.kernel)
         print(f"Saved kernel to file")
@@ -87,7 +87,7 @@ class ViabilityKernel:
         for m in range(self.n_modes):
             plt.figure()
             plt.title(f"Kernel phi: {phi} Mode: {m}")
-            img = self.kernel[:, :, phi_ind, m].T + self.constraints[:, :, phi_ind, m].T *2
+            img = self.kernel[:, :, phi_ind, m].T 
             plt.imshow(img, origin='lower')
         plt.show()
 
@@ -105,19 +105,23 @@ class ViabilityKernel:
         plt.show()
 
 # @njit(cache=True)
-def build_dynamics_table(phis, qs, velocity, t_step):
-    dynamics = np.zeros((len(phis), len(qs), 3))
-
+def build_dynamics_table(phis, qs, velocity, t_step, resolution):
+    dynamics = np.zeros((len(phis), len(qs), 3), dtype=np.int)
+    phi_range = np.pi
     for i, p in enumerate(phis):
         for j, m in enumerate(qs):
-            dynamics[i, j, 2] = p + m * t_step
-            dynamics[i, j, 0] = np.sin(dynamics[i, j, 2]) * velocity * t_step
-            dynamics[i, j, 1] = np.cos(dynamics[i, j, 2]) * velocity * t_step
+            phi = p + m * t_step
+            new_k = int(round((phi + phi_range/2) / phi_range * (len(phis)-1)))
+            dynamics[i, j, 2] = min(max(0, new_k), len(phis)-1)
+            dx = np.sin(phi) * velocity * t_step
+            dynamics[i, j, 0] = int(round(dx * resolution))
+            dy = np.cos(phi) * velocity * t_step
+            dynamics[i, j, 1] = int(round(dy * resolution))
 
     return dynamics
 
 # @jit(cache=True)
-def kernel_loop(kernel, xs, ys, phis, qs, mode_window, n_modes, dynamics, x_offset, y_offset, phi_range, resolution, half_block):
+def kernel_loop(kernel, xs, ys, phis, qs, mode_window, n_modes, dynamics):
     previous_kernel = np.copy(kernel)
     for i in range(len(xs)):
         for j in range(len(ys)):
@@ -125,36 +129,23 @@ def kernel_loop(kernel, xs, ys, phis, qs, mode_window, n_modes, dynamics, x_offs
                 for m in range(len(qs)):
                     if kernel[i, j, k, m] == 1:
                         continue 
-                    kernel[i, j, k, m] = check_kernel_state(xs[i], ys[j], k, m, mode_window, n_modes, dynamics, previous_kernel, xs, ys, phis, x_offset, y_offset, phi_range, resolution, half_block)
+                    kernel[i, j, k, m] = check_kernel_state(i, j, k, m, mode_window, n_modes, dynamics, previous_kernel, xs, ys)
 
     return kernel
 
-# @njit(cache=True)
-def check_kernel_state(x, y, k, m, mode_window, n_modes, dynamics, previous_kernel, xs, ys, phis, x_offset, y_offset, phi_range, resolution, half_block):
+@njit(cache=True)
+def check_kernel_state(i, j, k, m, mode_window, n_modes, dynamics, previous_kernel, xs, ys):
     min_m = max(0, m-mode_window)
     max_m = min(n_modes, m+mode_window+1)
     for l in range(min_m, max_m):
-        dx, dy, new_phi = dynamics[k, m, :]
-        new_x, new_y = x + dx, y + dy
-        if new_phi <= -np.pi/2 or new_phi >= np.pi/2:
-            continue
+        di, dj, new_k = dynamics[k, m, :]
+        new_i = min(max(0, i + di), len(xs)-1)  
+        new_j = min(max(0, j + dj), len(ys)-1)
 
-        kernal_inds = convert_state_to_kernel(new_x, new_y, new_phi, xs, ys, phis, x_offset, y_offset, phi_range, resolution, half_block)
-        if not previous_kernel[kernal_inds[0], kernal_inds[1], kernal_inds[2], l] == 1:
-            return False 
+        if not previous_kernel[new_i, new_j, new_k, l]:
+            return False
+
     return True
-
-#TODO: remove this with better dynamics table usage.
-def convert_state_to_kernel(x, y, phi, xs, ys, phis, x_offset, y_offset, phi_range, resolution, half_block):
-    # x_ind = int(round((x+self.half_block-self.x_offset)*self.resolution))
-    x_ind = int(round((x-x_offset)*resolution))
-    x_ind = max(0, min(x_ind, len(xs)-1))
-    y_ind = int((y+half_block-y_offset)*resolution)
-    y_ind = max(0, min(y_ind, len(ys)-1))
-    phi_ind = int(round((phi + phi_range/2) / phi_range * (len(phis)-1)))
-
-    return (x_ind, y_ind, phi_ind)
- 
 
 
 def run_original():
@@ -165,12 +156,13 @@ def run_original():
     # viab.view_kernel(-0.2)
     # viab.view_kernel(0)
     # viab.view_kernel(0.2)
-    # viab.view_all_modes(0)
+    viab.view_all_modes(0)
 
 
 
 if __name__ == "__main__":
-    t = timeit.timeit(run_original, number=1)
-    print(f"Time taken: {t}")
+    # t = timeit.timeit(run_original, number=1)
+    # print(f"Time taken: {t}")
+    run_original()
 
 
